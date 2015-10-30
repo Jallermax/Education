12f0bb478f97e4942a93fa38bff64f2ab8740857
Jallermax <jallermax@gmail.com>
2015-10-30 14:57:30 +0300

    0.4 Timer added

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@
  */
 public class Main {
     public static void main(String[] args) {
-        Visual.result(Task3.run(13195));//13195, 600851475143L
+        Visual.result(Task3.run(600851475143L));//13195, 600851475143L
         Visual.result(Task4.run());
         Visual.result(Task5.run(20));
     }
